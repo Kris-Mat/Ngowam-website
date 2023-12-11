5e4c5e9ee1a5af041e6cdffd6dd0cdc75e9fba00
Kris-Mat <matsila.krista@gmail.com>
2023-12-11 21:07:07 +0200

    figuring out the navbar animation

--- script/animations.js
+++ script/animations.js
@@ -1,7 +1,6 @@
 // animation to see more information about the services
 $(document).ready(function () {
-
-  // hide everything 
+  // hide everything
   $(".home-services-info").hide();
   $(".home-services-info").css("opacity", 0);
 
@@ -9,11 +8,13 @@ $(document).ready(function () {
   $("#home-services-info-consulting").show();
   $("#home-services-info-consulting").css("opacity", 1);
 
-  
   jQuery(".home-services-name").on("click", function () {
     var click = false;
 
-    if($(this).siblings(".home-services-info").is(":visible") === true && click === false) {
+    if (
+      $(this).siblings(".home-services-info").is(":visible") === true &&
+      click === false
+    ) {
       click = true;
       console.log("show");
       $(this).siblings(".home-services-info").animate(
@@ -25,9 +26,12 @@ $(document).ready(function () {
       $(this).siblings(".home-services-info").hide(200);
     }
 
-    if ($(this).siblings(".home-services-info").is(":visible") === false && click === false) {
+    if (
+      $(this).siblings(".home-services-info").is(":visible") === false &&
+      click === false
+    ) {
       click = true;
-      
+
       // hide everything else first
       $(".home-services-info").animate(
         {
@@ -36,7 +40,7 @@ $(document).ready(function () {
         50
       );
       $(".home-services-info").hide(200);
-  
+
       // now show what you want to see
       $(this).siblings(".home-services-info").show(200);
       $(this).siblings(".home-services-info").animate(
@@ -45,8 +49,42 @@ $(document).ready(function () {
         },
         300
       );
-    } 
+    }
   });
+});
+
+// animation to show the navbar or not
+// keep track of previous scroll position
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+  // current scroll position
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    // user has scrolled up
+    $("nav").show(200);
+    $("nav").animate(
+      {
+        opacity: 1,
+        height: 150,
+      },
+      300
+    );
+  } else {
+    // user has scrolled down
+    $("nav").animate(
+      {
+        opacity: 0,
+        height: 0,
+      },
+      50
+    );
+    $("nav").hide(200);
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
 });
 
 // jQuery('.home-services-info-container').on('click', function () {
